4e8c96f1d49e62a8ba7e45f3f4a67201c9774c4e
StevenASam <steven.sam68@gmail.com>
2021-01-10 01:46:34 -0800

    Added integration to Firebase and created test

--- app/src/main/java/com/example/recyclingcalculator/ui/MainActivity.java
+++ app/src/main/java/com/example/recyclingcalculator/ui/MainActivity.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.recyclingcalculator.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupWidgets();
+        start();
     }
 
     private void setupWidgets() {
@@ -34,5 +41,17 @@ public class MainActivity extends AppCompatActivity {
             Intent loadFormIntent = LoadFormActivity.makeIntent(MainActivity.this);
             startActivity(loadFormIntent);
         });
+    }
+
+
+    private void start(){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        HashMap<String, Object> users = new HashMap<>();
+        Test t = new Test(a,b,c);
+        users.put("Steven",t);
+        users.put("Matt","test2");
+        FirebaseDatabase.getInstance().getReference().child("saved-list").child("name").setValue(users);
     }
 }
